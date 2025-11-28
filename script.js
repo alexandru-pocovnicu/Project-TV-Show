255ffd20ad2e1cc83e214792e01c2ba0728427c0
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-28 20:05:27 +0000

    got the variables name,season,number,summary,image for a single episode

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+const {name,season,number,summary,image}=getOneEpisode()
+const mediumSizedImage=image.medium
+
+console.log(name, season, number,mediumSizedImage);

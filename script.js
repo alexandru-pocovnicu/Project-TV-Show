6f144a2ddb23639a17fc03d943742e9da941d40b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-28 21:16:49 +0000

    style: format code for better readability and consistency

--- script.js
+++ script.js
@@ -11,15 +11,16 @@ function makePageForEpisodes(episodeList) {
 
 window.onload = setup;
 
-const {name,season,number,summary,image}=getOneEpisode()
-const mediumSizedImage=image.medium
-const paddedSeason=String(season).padStart(2,"0")
-const paddedNumber=String(number).padStart(2,"0")
-const episodeCode=`S${paddedSeason}E${paddedNumber}`
+const { name, season, number, summary, image } = getOneEpisode();
+const mediumSizedImage = image.medium;
+const paddedSeason = String(season).padStart(2, "0");
+const paddedNumber = String(number).padStart(2, "0");
+const episodeCode = `S${paddedSeason}E${paddedNumber}`;
 
-const episode=document.getElementById("episode").content.cloneNode(true)
-episode.querySelector("#name").textContent=name
+const episode = document.getElementById("episode").content.cloneNode(true);
+episode.querySelector("#name").textContent = name;
 episode.querySelector("#episode-code").textContent = episodeCode;
 episode.querySelector("#image").src = mediumSizedImage;
 episode.querySelector("#summary").textContent = summary;
+document.querySelector("#root").appendChild(episode)
 console.log(episode);

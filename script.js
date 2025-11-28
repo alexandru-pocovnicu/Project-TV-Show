0ca437176f9f85a6691abb84486d760f8bf214a3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-28 20:10:16 +0000

    created episode code

--- script.js
+++ script.js
@@ -13,5 +13,8 @@ window.onload = setup;
 
 const {name,season,number,summary,image}=getOneEpisode()
 const mediumSizedImage=image.medium
+const paddedSeason=String(season).padStart(2,"0")
+const paddedNumber=String(number).padStart(2,"0")
+const episodeCode=`S${paddedSeason}E${paddedNumber}`
 
-console.log(name, season, number,mediumSizedImage);
+console.log(episodeCode);

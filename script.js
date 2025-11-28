1d3d7d95a672aaeaeae6adbc47fe7335fc35a30e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-28 21:02:45 +0000

    updated the template with the episode details

--- script.js
+++ script.js
@@ -17,4 +17,9 @@ const paddedSeason=String(season).padStart(2,"0")
 const paddedNumber=String(number).padStart(2,"0")
 const episodeCode=`S${paddedSeason}E${paddedNumber}`
 
-console.log(episodeCode);
+const episode=document.getElementById("episode").content.cloneNode(true)
+episode.querySelector("#name").textContent=name
+episode.querySelector("#episode-code").textContent = episodeCode;
+episode.querySelector("#image").src = mediumSizedImage;
+episode.querySelector("#summary").textContent = summary;
+console.log(episode);

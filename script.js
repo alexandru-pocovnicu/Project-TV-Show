1c4eaa1ed630710317f09279afde224ff9d6e135
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-29 16:18:54 +0000

    streamline setup function, remove redundant code and added footer

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-//You can edit ALL of the code here
-// function setup() {
-//   const allEpisodes = getAllEpisodes();
-//   makePageForEpisodes(allEpisodes);
-// }
+function setup(card) {
+  //extracts the variables name, season, number, summary, image from episodes.js
+  const { name, season, number, summary, image } = card;
+  const mediumSizedImage = image.medium;
+  const paddedSeason = String(season).padStart(2, "0");
+  const paddedNumber = String(number).padStart(2, "0");
+  const episodeCode = `S${paddedSeason}E${paddedNumber}`;
 
-function makePageForEpisodes(episodeList) {
-   const rootElem = document.getElementById("root");
-   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //cloned the html template, update template content and updates it to the DOM
+  const episode = document.getElementById("episode").content.cloneNode(true);
+  episode.querySelector("#name").textContent = name;
+  episode.querySelector("#episode-code").textContent = episodeCode;
+  episode.querySelector("#image").src = mediumSizedImage;
+  episode.querySelector("#summary").innerHTML = summary;
+  document.querySelector("#root").appendChild(episode);
 }
 
-window.onload = setup;
+const allEpisodes = getAllEpisodes();
+allEpisodes.map(setup);
 
-function setup(card){
-//extracts the variables name, season, number, summary, image from episodes.js
-const { name, season, number, summary, image } = card;
-const mediumSizedImage = image.medium;
-const paddedSeason = String(season).padStart(2, "0");
-const paddedNumber = String(number).padStart(2, "0");
-const episodeCode = `S${paddedSeason}E${paddedNumber}`;
-
-//cloned the html template, update template content and updates it to the DOM
-const episode = document.getElementById("episode").content.cloneNode(true);
-episode.querySelector("#name").textContent = name;
-episode.querySelector("#episode-code").textContent = episodeCode;
-episode.querySelector("#image").src = mediumSizedImage;
-episode.querySelector("#summary").textContent = summary;
-document.querySelector("body").appendChild(episode)
-console.log(summary);
-
-}
-const allEpisodes=getAllEpisodes()
-allEpisodes.map(setup)
+//create footer and append to the body
+const footer = document.createElement("footer");
+footer.innerHTML =
+  'Data originally from:<a href="https://www.tvmaze.com/">TVMaze.com</a>';
+document.body.append(footer);

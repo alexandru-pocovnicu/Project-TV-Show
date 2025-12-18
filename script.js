12f85ee4649a588c6a21dabff1f7b357e61f2290
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-12-18 15:51:20 +0000

    formatted code for better readability

--- script.js
+++ script.js
@@ -26,26 +26,26 @@ const state = {
   episodes: [],
   searchTerm: "",
   selectedEpisodeId: "",
-  loading:true,
-  error:null,
-  
+  loading: true,
+  error: null,
 };
 
 fetch("https://api.tvmaze.com/shows/82/episodes")
-  .then((response) =>{ 
-    if(!response.ok)throw new Error("Error")
-    return response.json()})
+  .then((response) => {
+    if (!response.ok) throw new Error("Error");
+    return response.json();
+  })
   .then((data) => {
     state.episodes = data;
-    state.loading=false
+    state.loading = false;
     render();
     populateEpisodeSelect();
   })
-  .catch((err)=>{
+  .catch((err) => {
     state.loading = false;
     state.error = "Failed to load episodes. Please try again later.";
     render();
-  })
+  });
 
 function formatEpisodeCode(ep) {
   const paddedSeason = String(ep.season).padStart(2, "0");
@@ -82,7 +82,6 @@ function render() {
   const counterElem = document.querySelector("#counter");
   rootElem.textContent = "";
 
-
   if (state.error) {
     rootElem.textContent = state.error;
     if (counterElem) counterElem.textContent = "";
@@ -97,7 +96,6 @@ function render() {
 
   // Start from all episodes
   let visibleEpisodes = state.episodes;
-
 
   // If an episode is selected, show ONLY that one
   if (state.selectedEpisodeId !== "") {
@@ -115,7 +113,6 @@ function render() {
       );
     });
   }
-
 
   if (visibleEpisodes.length === 0) {
     rootElem.textContent = "No episodes found";

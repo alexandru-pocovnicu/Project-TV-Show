68ba47f2a6ed0fe288f7f68c2021e2eb19cf1179
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-05 20:48:09 +0100

    moved the info for each episode in to it's own dedicated function, created dom elements and appended them to a dedicated container

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 //You can edit ALL of the code here
 
-
-
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  for(let episode of episodeList){
-    let {name,number,season,summary,image}=episode
-    let seasonNumber=`S${season.toString().padStart(2,"0")}`
-    let episodeNumber=`E${number.toString().padStart(2,"0")}`
-    let episodeCode=seasonNumber+episodeNumber
-    let mediumImage=image.medium
-    console.log(episodeCode);
-    
-  }
+  
+}
+
+function createEpisodeCard(episode) {
+  let { name, number, season, summary, image } = episode;
+
+  let seasonNumber = `S${season.toString().padStart(2, "0")}`;
+  let episodeNumber = `E${number.toString().padStart(2, "0")}`;
+  let episodeCode = seasonNumber + episodeNumber;
+  let episodeTitle = document.createElement("h2");
+  episodeTitle.textContent = `${name}-${episodeCode}`;
+
+  let episodeSummary=document.createElement("p")
+  episodeSummary.innerHTML=summary
+
+  let mediumImage =document.createElement("img")
+  mediumImage.src=image.medium;
+  mediumImage.alt = `${name}-${episodeCode}`;
+
+  let episodeContainer=document.createElement("container")
+  episodeContainer.append(episodeTitle, mediumImage, episodeSummary);
+
+  return episodeContainer
 }
 
 window.onload = setup;

04e2ad7385688a882884cce6c008e55e97d0b7f4
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-05 19:25:28 +0100

    extracted info and assigned to variables for each episode

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 //You can edit ALL of the code here
+
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for(let episode of episodeList){
+    let {name,number,season,summary,image}=episode
+    let seasonNumber=`S${season.toString().padStart(2,"0")}`
+    let episodeNumber=`E${number.toString().padStart(2,"0")}`
+    let episodeCode=seasonNumber+episodeNumber
+    let mediumImage=image.medium
+    console.log(episodeCode);
+    
+  }
 }
 
 window.onload = setup;
